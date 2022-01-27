369cfc8312fbf23714eb0754bf7e26e0823f79fe
mfrancia <matteo.francia2@studio.unibo.it>
2022-01-27 12:59:47 +0100

    working on permissions

--- db-migration/dockerimages/oracle11g/f02_oracle_init.sh
+++ db-migration/dockerimages/oracle11g/f02_oracle_init.sh
@@ -5,6 +5,6 @@
 chmod -R 777 /data
 ls -las /data
 impdp covid_weekly/oracle@127.0.0.1:1521/xe DIRECTORY=oracle_dump DUMPFILE=COVID_WEEKLY.DMP SCHEMAS=covid_weekly
-impdp foodmart/oracle@127.0.0.1:1521/xe DIRECTORY=oracle_dump DUMPFILE=FOODMART.DMP SCHEMAS=SCHEMAS
+impdp foodmart/oracle@127.0.0.1:1521/xe DIRECTORY=oracle_dump DUMPFILE=FOODMART.DMP SCHEMAS=foodmart
 impdp frenchelectricity/oracle@127.0.0.1:1521/xe DIRECTORY=oracle_dump DUMPFILE=FRENCHELECTRICITY.DMP SCHEMAS=frenchelectricity
 impdp frenchelectricityext/oracle@127.0.0.1:1521/xe DIRECTORY=oracle_dump DUMPFILE=FRENCHELECTRICITYEXT.DMP SCHEMAS=frenchelectricityext
