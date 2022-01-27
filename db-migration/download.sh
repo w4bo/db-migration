9f0a4dbe63f07d2ebc84b7defd7e23acce709796
mfrancia <matteo.francia2@studio.unibo.it>
2022-01-27 14:16:36 +0100

    working on permissions

--- db-migration/download.sh
+++ db-migration/download.sh
@@ -1,3 +1,4 @@
+set -e
 cd resources
 curl -o foodmart-mysql.sql http://big.csr.unibo.it/projects/nosql-datasets/foodmart-mysql.sql
 curl -o foodmart-mysql-schema.sql http://big.csr.unibo.it/projects/nosql-datasets/foodmart-mysql-schema.sql
