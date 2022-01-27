22d1c42a29de3c5702340df0a280a2b1ba837e2b
mfrancia <matteo.francia2@studio.unibo.it>
2022-01-27 12:59:34 +0100

    working on permissions

--- db-migration/dockerimages/oracle11g/f02_oracle_init.sh
+++ db-migration/dockerimages/oracle11g/f02_oracle_init.sh
@@ -4,7 +4,7 @@
 # expdp foodmart/oracle@research directory=oracle_dump dumpfile=foodmart.dmp SCHEMAS=foodmart
 chmod -R 777 /data
 ls -las /data
-impdp covid_weekly/oracle@127.0.0.1:1521/xe directory=oracle_dump dumpfile=COVID_WEEKLY.DMP
-impdp foodmart/oracle@127.0.0.1:1521/xe directory=oracle_dump dumpfile=FOODMART.DMP
-impdp frenchelectricity/oracle@127.0.0.1:1521/xe directory=oracle_dump dumpfile=FRENCHELECTRICITY.DMP
-impdp frenchelectricityext/oracle@127.0.0.1:1521/xe directory=oracle_dump dumpfile=FRENCHELECTRICITYEXT.DMP
+impdp covid_weekly/oracle@127.0.0.1:1521/xe DIRECTORY=oracle_dump DUMPFILE=COVID_WEEKLY.DMP SCHEMAS=covid_weekly
+impdp foodmart/oracle@127.0.0.1:1521/xe DIRECTORY=oracle_dump DUMPFILE=FOODMART.DMP SCHEMAS=SCHEMAS
+impdp frenchelectricity/oracle@127.0.0.1:1521/xe DIRECTORY=oracle_dump DUMPFILE=FRENCHELECTRICITY.DMP SCHEMAS=frenchelectricity
+impdp frenchelectricityext/oracle@127.0.0.1:1521/xe DIRECTORY=oracle_dump DUMPFILE=FRENCHELECTRICITYEXT.DMP SCHEMAS=frenchelectricityext
